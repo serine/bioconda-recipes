756f6b1add68fa77f7d2c2953032f9e76ceea8d4
serine <tskiril@gmail.com>
2018-04-20 14:30:57 +1000

    Changed golang to be from conda-forge, official channel and bumped build version. Also bds now installs in /opt

--- recipes/bigdatascript/build.sh
+++ recipes/bigdatascript/build.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-set -e
-set -o pipefail
-set -x
 
-GOLANG_VERSION=1.9.2
-
-# We download a throwaway Go compiler, just for the BDS build environment
-if [ `uname` == Darwin ]; then
-  ARCH=darwin-amd64
-else
-  ARCH=linux-amd64
-fi
-
-GOTMP=$(mktemp -d)
-wget -O  "${GOTMP}/go.tar.gz" "https://dl.google.com/go/go${GOLANG_VERSION}.${ARCH}.tar.gz" || \
-  curl "https://dl.google.com/go/go${GOLANG_VERSION}.${ARCH}.tar.gz" >"${GOTMP}/go.tar.gz"
-tar xvzf "${GOTMP}/go.tar.gz" -C "${GOTMP}"
-
-PATH="${PATH}:${GOTMP}/go/bin" BDS_HOME="${PREFIX}/bds" scripts/install.sh
-
-rm -rf "${GOTMP}"
+BDS_HOME="${PREFIX}/opt/bds" scripts/install.sh
 
 mkdir -p "${PREFIX}/bin"
-ln -s "${PREFIX}/bds/bds" "${PREFIX}/bin/bds"
-#This is tmp hack, for RNAsik to work. It needs bds.config next to bds exec
-ln -s "${PREFIX}/bds/bds.config" "${PREFIX}/bin/bds.config"
-chmod 0755 "${PREFIX}/bds/bds"
+ln -s "${PREFIX}/opt/bds/bds" "${PREFIX}/bin/bds"
+ln -s "${PREFIX}/opt/bds/bds.config" "${PREFIX}/bin/bds.config"
+chmod 0755 "${PREFIX}/opt/bds/bds"
