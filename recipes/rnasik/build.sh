6925d283115a48005b8f61d0cbda12c9a3e33173
serine <tskiril@gmail.com>
2018-04-20 14:32:00 +1000

    RNAsik installs into /opt now, also bumped build version

--- recipes/rnasik/build.sh
+++ recipes/rnasik/build.sh
@@ -3,11 +3,11 @@
 sik_version=${PKG_VERSION}
 
 mkdir -p "${PREFIX}/bin"
-mkdir -p "${PREFIX}/rnasik-${sik_version}"
-sed -i "s/src\/RNAsik.bds/rnasik-${sik_version}\/src\/RNAsik.bds/" bin/RNAsik
-cp -r "bin" "${PREFIX}/rnasik-${sik_version}"
-cp -r "src" "${PREFIX}/rnasik-${sik_version}"
-cp -r "scripts" "${PREFIX}/rnasik-${sik_version}"
-cp -r "configs" "${PREFIX}/rnasik-${sik_version}"
-cp -r "supplementary" "${PREFIX}/rnasik-${sik_version}"
-ln -s "${PREFIX}/rnasik-${sik_version}/bin/RNAsik" "${PREFIX}/bin/RNAsik"
+mkdir -p "${PREFIX}/opt/rnasik-${sik_version}"
+sed -i "s/src\/RNAsik.bds/opt\/rnasik-${sik_version}\/src\/RNAsik.bds/" bin/RNAsik
+cp -r "bin" "${PREFIX}/opt/rnasik-${sik_version}"
+cp -r "src" "${PREFIX}/opt/rnasik-${sik_version}"
+cp -r "scripts" "${PREFIX}/opt/rnasik-${sik_version}"
+cp -r "configs" "${PREFIX}/opt/rnasik-${sik_version}"
+cp -r "supplementary" "${PREFIX}/opt/rnasik-${sik_version}"
+ln -s "${PREFIX}/opt/rnasik-${sik_version}/bin/RNAsik" "${PREFIX}/bin/RNAsik"
